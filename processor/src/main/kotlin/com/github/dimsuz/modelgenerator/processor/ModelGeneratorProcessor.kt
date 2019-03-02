132831f51eb33685b225ff278929020b6057306b
Dmitry Suzdalev <dimsuz@gmail.com>
2019-03-02 18:55:21 +0200

    Add simple processor implementation, make it discoverable

--- processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelGeneratorProcessor.kt
+++ processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelGeneratorProcessor.kt
@@ -1,4 +1,37 @@
 package com.github.dimsuz.modelgenerator.processor
 
-class ModelGeneratorProcessor {
+import com.github.dimsuz.modelgenerator.annotation.ReactiveModel
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.ProcessingEnvironment
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.TypeElement
+
+class ModelGeneratorProcessor : AbstractProcessor() {
+  private lateinit var logger: Logger
+
+  override fun init(processingEnv: ProcessingEnvironment) {
+    super.init(processingEnv)
+
+    logger = Logger(processingEnv.messager)
+  }
+
+  override fun getSupportedAnnotationTypes(): Set<String> {
+    return setOf(ReactiveModel::class.java.canonicalName)
+  }
+
+  override fun getSupportedSourceVersion(): SourceVersion {
+    return SourceVersion.latest()
+  }
+
+  override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
+    for (element in roundEnv.getElementsAnnotatedWith(ReactiveModel::class.java)) {
+      if (element.kind != ElementKind.CLASS) {
+        logger.error("${ReactiveModel::class.java.simpleName} can only be applied to interfaces")
+        return true
+      }
+    }
+    return true
+  }
 }
