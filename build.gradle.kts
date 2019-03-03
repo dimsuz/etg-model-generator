508536589334d2d52811dcc4a86be9dd5652a375
Dmitry Suzdalev <dimsuz@gmail.com>
2019-03-03 13:49:44 +0200

    Add versions and deps properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,45 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    java
-    kotlin("jvm") version "1.3.21"
+buildscript {
+  var versions: Map<String, String> by extra
+  @Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
+  var deps: Map<String, String> by extra
+
+  versions = mapOf(
+    "kotlin" to "1.3.21", // see also plugin block below
+    "rxJava" to "2.2.7"
+  )
+  @Suppress("UNUSED_VALUE")
+  deps = mapOf(
+    "rxJava" to "io.reactivex.rxjava2:rxjava:${versions["rxJava"]}"
+  )
 }
 
-allprojects {
-    group = "com.github.dimsuz"
-    version = "1.0-SNAPSHOT"
+plugins {
+  java
+  kotlin("jvm") version "1.3.21"
+}
 
-    repositories {
-        mavenCentral()
-        jcenter()
-    }
+
+allprojects {
+  group = "com.github.dimsuz"
+  version = "1.0-SNAPSHOT"
+
+  repositories {
+    mavenCentral()
+    jcenter()
+  }
 }
 
 subprojects {
-    apply {
-        plugin("kotlin")
-    }
-    dependencies {
-        compile(kotlin("stdlib-jdk8"))
-        testCompile("junit", "junit", "4.12")
-    }
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+  apply {
+    plugin("kotlin")
+  }
+  dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+  }
+  tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+  }
 }
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -7,8 +7,11 @@ application {
   mainClassName = "com.github.dimsuz.modelgenerator.sample.SampleModelKt"
 }
 
+val deps: Map<String, String> by rootProject.extra
+
 dependencies {
   implementation(project(":runtime"))
+  implementation(deps.getValue("rxJava"))
 
   kapt(project(":processor"))
 }
