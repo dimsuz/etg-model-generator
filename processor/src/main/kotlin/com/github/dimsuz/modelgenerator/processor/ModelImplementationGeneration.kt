dbf404441430e7f5366274895530255e0c656377
Dmitry Suzdalev <dimsuz@gmail.com>
2019-03-07 00:39:54 +0200

    Generate schedulingRequest model impl function

--- processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelImplementationGeneration.kt
+++ processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelImplementationGeneration.kt
@@ -1,5 +1,6 @@
 package com.github.dimsuz.modelgenerator.processor
 
+import com.github.dimsuz.modelgenerator.SchedulingSettings
 import com.github.dimsuz.modelgenerator.processor.entity.Either
 import com.github.dimsuz.modelgenerator.processor.entity.ReactiveModelDescription
 import com.github.dimsuz.modelgenerator.processor.util.enclosingPackageName
@@ -11,9 +12,12 @@ import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.TypeVariableName
 import com.squareup.kotlinpoet.asClassName
+import io.reactivex.subjects.PublishSubject
 import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.element.ExecutableElement
 
@@ -25,6 +29,9 @@ internal fun generateModelImplementation(
   val modelElement = modelDescription.modelElement
   val modelName = modelElement.simpleName.toString()
   val className = modelName + "Impl"
+  val requestType = createRequestType()
+  val requestStreamProp = createRequestStreamProp(requestType)
+  val scheduleRequestFun = createScheduleRequestFun(requestType, requestStreamProp)
   val fileSpec = FileSpec
     .builder(modelElement.enclosingPackageName, "$className.kt")
     .addType(
@@ -33,18 +40,31 @@ internal fun generateModelImplementation(
           PropertySpec.builder(
             OPERATIONS_PROPERTY_NAME,
             operations
+          ).addModifiers(KModifier.PRIVATE).build(),
+          PropertySpec.builder(
+            SCHEDULING_PROPERTY_NAME,
+            SchedulingSettings::class
           ).addModifiers(KModifier.PRIVATE).build()
         )
         .addSuperinterface(modelElement.asClassName())
         .addModifiers(KModifier.INTERNAL)
+        .addProperty(requestStreamProp)
+        .addFunction(scheduleRequestFun)
         .addFunctions(modelDescription.nonReactiveMethods.map { generateNonReactiveMethodImpl(it) })
+        .addType(requestType)
         .build()
     )
     .build()
   return writeFile(processingEnv, fileSpec)
 }
 
-fun generateNonReactiveMethodImpl(element: ExecutableElement): FunSpec {
+private fun createRequestStreamProp(requestType: TypeSpec): PropertySpec {
+  return PropertySpec.builder("requestStream", PublishSubject::class.asClassName().parameterizedBy(ClassName.bestGuess(requestType.name!!)), KModifier.PRIVATE)
+    .initializer("PublishSubject.create<%T>()", TypeVariableName(requestType.name!!))
+    .build()
+}
+
+private fun generateNonReactiveMethodImpl(element: ExecutableElement): FunSpec {
   return FunSpec.overridingWrapper(element)
     .addCode(
       CodeBlock.builder()
@@ -57,4 +77,23 @@ fun generateNonReactiveMethodImpl(element: ExecutableElement): FunSpec {
     .build()
 }
 
+private fun createScheduleRequestFun(
+  requestType: TypeSpec,
+  requestStreamProp: PropertySpec
+): FunSpec {
+  return FunSpec.builder("scheduleRequest")
+    .addModifiers(KModifier.PRIVATE)
+    .addParameter("request", ClassName.bestGuess(requestType.name!!))
+    .addStatement("$SCHEDULING_PROPERTY_NAME.checkIsOnUiThread()")
+    .addStatement("%N.onNext(%N)", requestStreamProp, "request")
+    .build()
+}
+
+private fun createRequestType(): TypeSpec {
+  return TypeSpec.classBuilder("Request")
+    .addModifiers(KModifier.SEALED, KModifier.PRIVATE)
+    .build()
+}
+
 private const val OPERATIONS_PROPERTY_NAME = "operations"
+private const val SCHEDULING_PROPERTY_NAME = "schedulingSettings"
