35e9e3e5e117e79b26d8925837be279263e58154
Dmitry Suzdalev <dz@appkode.ru>
2019-03-07 19:46:21 +0200

    Generate much of implementation

--- processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelImplementationGeneration.kt
+++ processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/ModelImplementationGeneration.kt
@@ -15,8 +15,8 @@ import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.TypeVariableName
 import com.squareup.kotlinpoet.asClassName
+import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.element.ExecutableElement
@@ -28,12 +28,15 @@ internal fun generateModelImplementation(
 ): Either<String, Unit> {
   val modelElement = modelDescription.modelElement
   val modelName = modelElement.simpleName.toString()
-  val className = modelName + "Impl"
-  val requestType = createRequestType()
-  val requestStreamProp = createRequestStreamProp(requestType)
-  val scheduleRequestFun = createScheduleRequestFun(requestType, requestStreamProp)
+  val className = ClassName(modelElement.enclosingPackageName, modelName + "Impl")
+  val stateClassName = className.nestedClass("State")
+  val requestClassName = className.nestedClass("Request")
+  val actionClassName = className.nestedClass("Action")
+  val requestStreamProp = createRequestStreamProp(requestClassName)
+  val scheduleRequestFun = createScheduleRequestFun(requestClassName, requestStreamProp)
+  val constructorCode = createConstructorBody(requestStreamProp)
   val fileSpec = FileSpec
-    .builder(modelElement.enclosingPackageName, "$className.kt")
+    .builder(modelElement.enclosingPackageName, "${className.simpleName}.kt")
     .addType(
       TypeSpec.classBuilder(className)
         .primaryConstructor(
@@ -44,23 +47,69 @@ internal fun generateModelImplementation(
           PropertySpec.builder(
             SCHEDULING_PROPERTY_NAME,
             SchedulingSettings::class
-          ).addModifiers(KModifier.PRIVATE).build()
+          ).addModifiers(KModifier.PRIVATE).build(),
+          code = constructorCode
         )
         .addSuperinterface(modelElement.asClassName())
         .addModifiers(KModifier.INTERNAL)
         .addProperty(requestStreamProp)
+        .addProperty(PropertySpec
+          .builder(STATE_CHANGES_PROPERTY_NAME, Observable::class.java.asClassName().parameterizedBy(stateClassName), KModifier.PRIVATE)
+          .build())
+        .addProperty(PropertySpec
+          .builder(LAST_STATE_PROPERTY_NAME, stateClassName, KModifier.PRIVATE)
+          .mutable(true)
+          .addAnnotation(Volatile::class)
+          .initializer("%T()", stateClassName)
+          .build())
         .addFunction(scheduleRequestFun)
         .addFunctions(modelDescription.nonReactiveMethods.map { generateNonReactiveMethodImpl(it) })
-        .addType(requestType)
+        .addFunction(createCommandTemplateMethod(requestClassName, stateClassName, actionClassName))
+        .addFunction(createReduceStateMethod(stateClassName, actionClassName))
+        .addType(TypeSpec.classBuilder(requestClassName)
+          .addModifiers(KModifier.PRIVATE)
+          .build())
+        .addType(TypeSpec.classBuilder(stateClassName)
+          .addModifiers(KModifier.PRIVATE)
+          .build())
+        .addType(TypeSpec.classBuilder(actionClassName)
+          .addModifiers(KModifier.PRIVATE)
+          .build())
         .build()
     )
     .build()
   return writeFile(processingEnv, fileSpec)
 }
 
-private fun createRequestStreamProp(requestType: TypeSpec): PropertySpec {
-  return PropertySpec.builder("requestStream", PublishSubject::class.asClassName().parameterizedBy(ClassName.bestGuess(requestType.name!!)), KModifier.PRIVATE)
-    .initializer("PublishSubject.create<%T>()", TypeVariableName(requestType.name!!))
+private fun createConstructorBody(requestStreamProp: PropertySpec): CodeBlock {
+  return CodeBlock.of(
+    """
+    |$SCHEDULING_PROPERTY_NAME.checkIsOnUiThread()
+    |$STATE_CHANGES_PROPERTY_NAME = %N
+    |  .concatMap { request ->
+    |    $CREATE_COMMAND_FUN_NAME(request, $LAST_STATE_PROPERTY_NAME)
+    |      .map { r -> $REDUCE_STATE_FUN_NAME($LAST_STATE_PROPERTY_NAME, r) }
+    |      .doOnNext { $LAST_STATE_PROPERTY_NAME = it }
+    |  }
+    |  .share()
+    |stateChanges
+    |  .observeOn($SCHEDULING_PROPERTY_NAME.uiScheduler)
+    |  .subscribe(
+    |    {
+    |    },
+    |    { /* TODO LOG (it, "commands must not throw errors, rather set some error flag in state") */ })
+  """.trimMargin(),
+    requestStreamProp
+  )
+}
+
+private fun createRequestStreamProp(requestClassName: ClassName): PropertySpec {
+  return PropertySpec.builder(
+    "requestStream",
+    PublishSubject::class.asClassName().parameterizedBy(requestClassName),
+    KModifier.PRIVATE
+  )
+    .initializer("PublishSubject.create<%T>()", requestClassName)
     .build()
 }
 
@@ -78,12 +127,12 @@ private fun generateNonReactiveMethodImpl(element: ExecutableElement): FunSpec {
 }
 
 private fun createScheduleRequestFun(
-  requestType: TypeSpec,
+  requestClassName: ClassName,
   requestStreamProp: PropertySpec
 ): FunSpec {
   return FunSpec.builder("scheduleRequest")
     .addModifiers(KModifier.PRIVATE)
-    .addParameter("request", ClassName.bestGuess(requestType.name!!))
+    .addParameter("request", requestClassName)
     .addStatement("$SCHEDULING_PROPERTY_NAME.checkIsOnUiThread()")
     .addStatement("%N.onNext(%N)", requestStreamProp, "request")
     .build()
@@ -95,5 +144,29 @@ private fun createRequestType(): TypeSpec {
     .build()
 }
 
+private fun createCommandTemplateMethod(requestClassName: ClassName, stateClassName: ClassName, actionClassName: ClassName): FunSpec {
+  return FunSpec.builder(CREATE_COMMAND_FUN_NAME)
+    .addModifiers(KModifier.PRIVATE)
+    .addParameter("request", requestClassName)
+    .addParameter("state", stateClassName)
+    .addStatement("TODO()")
+    .returns(Observable::class.java.asClassName().parameterizedBy(actionClassName))
+    .build()
+}
+
+private fun createReduceStateMethod(stateClassName: ClassName, actionClassName: ClassName): FunSpec {
+  return FunSpec.builder(REDUCE_STATE_FUN_NAME)
+    .addModifiers(KModifier.PRIVATE)
+    .addParameter("previousState", stateClassName)
+    .addParameter("action", actionClassName)
+    .addStatement("return previousState")
+    .returns(stateClassName)
+    .build()
+}
+
 private const val OPERATIONS_PROPERTY_NAME = "operations"
 private const val SCHEDULING_PROPERTY_NAME = "schedulingSettings"
+private const val STATE_CHANGES_PROPERTY_NAME = "stateChanges"
+private const val LAST_STATE_PROPERTY_NAME = "lastState"
+private const val CREATE_COMMAND_FUN_NAME = "createCommand"
+private const val REDUCE_STATE_FUN_NAME = "reduceState"
--- processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/util/TypeSpecExtensions.kt
+++ processor/src/main/kotlin/com/github/dimsuz/modelgenerator/processor/util/TypeSpecExtensions.kt
@@ -1,15 +1,20 @@
 package com.github.dimsuz.modelgenerator.processor.util
 
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 
-internal fun TypeSpec.Builder.primaryConstructor(vararg properties: PropertySpec): TypeSpec.Builder {
+internal fun TypeSpec.Builder.primaryConstructor(
+  vararg properties: PropertySpec,
+  code: CodeBlock? = null
+): TypeSpec.Builder {
   val propertySpecs = properties.map { p -> p.toBuilder().initializer(p.name).build() }
   val parameters = propertySpecs.map { ParameterSpec.builder(it.name, it.type).build() }
   val constructor = FunSpec.constructorBuilder()
     .addParameters(parameters)
+    .apply { code?.let { this.addCode(it) } }
     .build()
 
   return this
